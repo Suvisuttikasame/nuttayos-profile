eb9136f4deafb859e28d961e03dd1e0ccac2010d
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2023-12-12 01:50:26 +0700

    - [feat] add detail

--- app/components/ExperienceCard.tsx
+++ app/components/ExperienceCard.tsx
@@ -16,15 +16,41 @@ export default function ExperienceCard({ clickedItem }: props) {
         </h3>
         <div>
           <span>responsibility :</span>
-          <p>content1</p>
+          <div className="pt-1">
+            <span>Part of software development department</span>
+            <ul className="flex flex-col gap-2 list-disc p-4">
+              <li>
+                manage project resources(time, team and bugget) in production
+                phase in order to be on schedule
+              </li>
+              <li>face problem sloving on production</li>
+              <li>recover plan whenever project behind schedule</li>
+              <li>test electrical system on sea before handover</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div
         className={`${
           clickedItem === "Hocco" ? "block" : "hidden"
-        } [transform:rotateY(180deg)] bg-[#FFF] shadow-md absolute w-full h-full [backface-visibility:hidden] rounded-md p-4`}
+        } [transform:rotateY(180deg)] bg-[#FFF] shadow-md absolute w-full h-full [backface-visibility:hidden] rounded-md p-4 grid grid-cols-2 items-center justify-items-center`}
       >
-        <p>picture1</p>
+        <div className="overflow-hidden rounded-md drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] w-32">
+          <Image
+            src="/asset/demo-pic.jpg"
+            alt="marsun-1"
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className="overflow-hidden rounded-md drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] w-32">
+          <Image
+            src="/asset/demo-pic.jpg"
+            alt="marsun-1"
+            width={150}
+            height={150}
+          />
+        </div>
       </div>
       <div
         className={`${
@@ -38,7 +64,18 @@ export default function ExperienceCard({ clickedItem }: props) {
         </h3>
         <div>
           <span>responsibility :</span>
-          <p>content2</p>
+          <div className="pt-1">
+            <span>Part of lean automation business department</span>
+            <ul className="flex flex-col gap-2 list-disc p-4">
+              <li>
+                manage project resources(time, team and bugget) in production
+                phase in order to be on schedule
+              </li>
+              <li>face problem sloving on production</li>
+              <li>recover plan whenever project behind schedule</li>
+              <li>test electrical system on sea before handover</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div
@@ -46,9 +83,24 @@ export default function ExperienceCard({ clickedItem }: props) {
           clickedItem === "Denso International Asia Co.,Ltd"
             ? "block"
             : "hidden"
-        } [transform:rotateY(180deg)] bg-[#FFF] shadow-md absolute w-full h-full [backface-visibility:hidden] rounded-md p-4`}
+        } [transform:rotateY(180deg)] bg-[#FFF] shadow-md absolute w-full h-full [backface-visibility:hidden] rounded-md p-4 grid grid-cols-2 items-center justify-items-center`}
       >
-        <p>picture2</p>
+        <div className="overflow-hidden rounded-md drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] w-32">
+          <Image
+            src="/asset/demo-pic.jpg"
+            alt="marsun-1"
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className="overflow-hidden rounded-md drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] w-32">
+          <Image
+            src="/asset/demo-pic.jpg"
+            alt="marsun-1"
+            width={150}
+            height={150}
+          />
+        </div>
       </div>
       <div
         className={`${
@@ -62,7 +114,18 @@ export default function ExperienceCard({ clickedItem }: props) {
         </h3>
         <div>
           <span>responsibility :</span>
-          <p>content3</p>
+          <div className="pt-1">
+            <span>Part of research and development department</span>
+            <ul className="flex flex-col gap-2 list-disc p-4">
+              <li>
+                manage project resources(time, team and bugget) in production
+                phase in order to be on schedule
+              </li>
+              <li>face problem sloving on production</li>
+              <li>recover plan whenever project behind schedule</li>
+              <li>test electrical system on sea before handover</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div
@@ -70,9 +133,24 @@ export default function ExperienceCard({ clickedItem }: props) {
           clickedItem === "Turnkey Communication Services Co.,LTD."
             ? "block"
             : "hidden"
-        } [transform:rotateY(180deg)] bg-[#FFF] shadow-md absolute w-full h-full [backface-visibility:hidden] rounded-md p-4`}
+        } [transform:rotateY(180deg)] bg-[#FFF] shadow-md absolute w-full h-full [backface-visibility:hidden] rounded-md p-4 grid grid-cols-2 items-center justify-items-center`}
       >
-        <p>picture3</p>
+        <div className="overflow-hidden rounded-md drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] w-32">
+          <Image
+            src="/asset/demo-pic.jpg"
+            alt="marsun-1"
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className="overflow-hidden rounded-md drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] w-32">
+          <Image
+            src="/asset/demo-pic.jpg"
+            alt="marsun-1"
+            width={150}
+            height={150}
+          />
+        </div>
       </div>
       <div
         className={`${
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,14 @@
 export default function Contact() {
-  return <main>contact</main>;
+  return (
+    <main className="text-xs">
+      <div className="w-1/2 h-64 bg-slate-100 mx-auto rounded-md shadow-md p-4">
+        <h1>My social media</h1>
+        <ul>
+          <li>email</li>
+          <li>linkedin</li>
+          <li>github</li>
+        </ul>
+      </div>
+    </main>
+  );
 }
