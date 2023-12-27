67c29bcfeb56c3acf2a656963d1cf3706bcd0beb
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2023-12-28 01:21:25 +0700

    - [doc] add personal content

--- app/components/PersonalInfo.tsx
+++ app/components/PersonalInfo.tsx
@@ -17,14 +17,33 @@ export default function PersonalInfo() {
           }}
         />
       </div>
-      <p className="p-2 w-3/4">
-        This is <span>Nuttayos Suvisuttikasame</span>, a full stack developer
-        with experience in warehouse system, system analysis, root cause
-        investigation and resource management. He has expertise in web
-        application development both backend(
-        <span>Nestjs</span>) and frontend(<span>React</span>) and also familar
-        with agile and waterfall methodology. He is intermidiate in CI,
-        Infra(Docker) and AWS.
+      <p className="p-2 w-3/4 text-sm text-justify indent-2.5 hyphens-auto">
+        This is <span className="font-bold">Nuttayos Suvisuttikasame</span>, a
+        full stack developer with experience in warehouse system, system
+        analysis, root cause investigation and resource management. I have
+        expertise in web application development both backend(
+        <span className="font-bold">Nestjs</span>) and frontend(
+        <span className="font-bold">React</span>) and also familar with agile
+        and waterfall methodology. Due to various perspectives on my journey,
+        that makes me always keep improving both soft and hard skills. Because I
+        believe we are born to be solution-makers, no matter which field you are
+        in, you&apos;ll face problems. I realize and ready to tackle it.
+        <br />
+        When not coding, I love playing football, collecting manga and usually
+        spending time on YouTube. Hopefully, I&apos;ll make many connections in
+        order to write a story together.
+        <br />
+        <span className="text-lg font-bold">Tools</span>
+        <br />
+        Client: React, Nextjs
+        <br />
+        Server: Nestjs, Nodejs
+        <br />
+        Database: PostgreSQL, MySQL
+        <br />
+        Infra: Docker
+        <br />
+        Manage: Jira, Asana
       </p>
     </div>
   );
