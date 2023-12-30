2bcb7f8f841b208ec35eee13f3a828270e6d3c34
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2023-12-30 22:33:46 +0700

    - [feat] add project info

---
+++ app/project/[projectName]/components/ProjectInfo.tsx
@@ -0,0 +1,39 @@
+type props = {
+  projectName: string;
+  projectDesc: {
+    detail: string;
+    stack: string;
+    feature: string[];
+    live: string;
+  };
+};
+
+export default function ProjectInfo({ projectName, projectDesc }: props) {
+  return (
+    <div className="w-1/2 max-[1100px]:w-3/4">
+      <h1 className="text-lg font-bold mb-4">{projectName}</h1>
+      <ul className="text-sm flex flex-col gap-3">
+        <li>
+          <span className="font-semibold">Detail : </span>
+          {projectDesc.detail}
+        </li>
+        <li>
+          <span className="font-semibold">Stack & Tools : </span>
+          {projectDesc.stack}
+        </li>
+        <li>
+          <span className="font-semibold">Feature : </span>
+          <ul className="ml-9 list-disc">
+            {projectDesc.feature.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        </li>
+        <li>
+          <span className="font-semibold">Live : </span>
+          {projectDesc.live}
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ app/project/[projectName]/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+import Image from "next/image";
+import ProjectInfo from "./components/ProjectInfo";
+import BackButton from "../components/BackButton";
+
+type props = {
+  params: { projectName: string };
+};
+type projectDescription = {
+  [projectName: string]: {
+    detail: string;
+    stack: string;
+    feature: string[];
+    live: string;
+  };
+};
+const project: projectDescription = {
+  "fast-pizza-co": {
+    detail:
+      "This project is a pizza shop simulation. For every customer who is hungry and a pizza lover, just go to a fast-pizza-co app then choose the type of pizza you love and let check it out. credit. Jonas Schmedtmann",
+    stack: "React, tailwin, redux-toolkit",
+    feature: [
+      "check in a customer name and store in redux",
+      "menu selection page to order pizza with quantity",
+      "checkout form with customer information",
+      "search order by order number",
+    ],
+    live: "-",
+  },
+  "world-wise-react": {
+    detail:
+      "This project is a small location tracking project from the React course. This app helps users who want to travel and have plans to set their activities and locations together on a map. So, whenever users want to check out details, just hit the pin on a map, and they can see the location that they've gone to in the past. credit. Jonas Schmedtmann",
+    stack: "React, css module, json-server",
+    feature: [
+      "login and store user data in a context api",
+      "submit location form to pin location on a map and save data in json-server",
+      "delete location that's pin on a map",
+    ],
+    live: "-",
+  },
+  Proshop: {
+    detail:
+      "This project is ecommerce platform project from MERN course in order to demonstrate how online shop work roughly regarding to both admin and client usage. credit. Brad Traversy",
+    stack: "React, Redux, Nodejs, Express and MongoDB",
+    feature: [
+      "both admin and client authentication",
+      "admin product management",
+      "admin user management",
+      "admin order management",
+      "client product review",
+      "client product search",
+      "client checkout process(payment method SCB QR code sandbox)",
+    ],
+    live: "-",
+  },
+  "smart-farm": {
+    detail:
+      "This is a smart farm project with drip irrigation system aim to help local people to harvest yields in dry area. The IoT system is brought to use to collecting farm status through Lora protocol (no mornitoring app) moreover we have microcontroller and floating sensor to control water level in a tank, and solenoid valve to open and close a tank punctually.",
+    stack:
+      "cubecell, Lora cat, floating sensor, solenoid valve, relay, water pump",
+    feature: [
+      "collecting farm status including pump running time, tank open status and water level on firebase",
+      "automatically fill in water in order to make sure that a tank does not have water lower than threshold",
+      "automatically drip water in a farm punctually",
+    ],
+    live: "-",
+  },
+};
+
+export default function SubProject({ params }: props) {
+  return (
+    <main className="flex flex-col gap-10 py-8 px-16 justify-center items-center">
+      <BackButton position="start" />
+      <ProjectInfo
+        projectName={params.projectName}
+        projectDesc={project[params.projectName]}
+      />
+      <div className="w-128 flex flex-col gap-2 py-4">
+        <Image
+          src="/asset/demo-pic.jpg"
+          alt="project-pic"
+          width={200}
+          height={100}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+          className="shadow-md rounded-md"
+        />
+        <Image
+          src="/asset/demo-pic.jpg"
+          alt="project-pic"
+          width={200}
+          height={100}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+          className="shadow-md rounded-md"
+        />
+      </div>
+    </main>
+  );
+}
